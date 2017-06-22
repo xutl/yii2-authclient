ea3de9eeff808be001eeb157be115d959742c9e5
xuchao <85825770@qq.com>
2017-06-22 12:32:23 +0800

    add test

---
+++ Yuncms.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+ namespace xutl\authclient;
+
+ use yii\authclient\OAuth2;
+
+ /**
+  * Test Oauth
+  * @package xutl\authclient
+  */
+ class Yuncms extends Oauth2
+ {
+     /**
+      * @inheritdoc
+      */
+     public $authUrl = 'http://api.yuncms.net/oauth2/authorize';
+
+     /**
+      * @inheritdoc
+      */
+     public $tokenUrl = 'http://api.yuncms.net/oauth2/token';
+
+     /**
+      * @inheritdoc
+      */
+     public $apiBaseUrl = 'http://api.yuncms.net/v1';
+
+     /**
+      * @inheritdoc
+      */
+     protected function initUserAttributes()
+     {
+         return $this->api('me', 'GET');
+     }
+
+     /**
+      * @inheritdoc
+      */
+     protected function defaultName()
+     {
+         return 'yuncms';
+     }
+
+     /**
+      * @inheritdoc
+      */
+     protected function defaultTitle()
+     {
+         return 'YUNCMS';
+     }
+ }
