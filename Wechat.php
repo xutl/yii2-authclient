e15802efd00684c15d956dc6fc21b05490fb4fc2
xuchao <85825770@qq.com>
2017-07-11 10:20:45 +0800

    将微信登录接口二合一

--- Wechat.php
+++ Wechat.php
@@ -8,6 +8,7 @@
 namespace xutl\authclient;
 
 use Yii;
+use yii\authclient\OAuthToken;
 use yii\base\Exception;
 use yii\authclient\OAuth2;
 
@@ -30,7 +31,22 @@ class Wechat extends OAuth2
     /**
      * @inheritdoc
      */
+    public $refreshTokenUrl = 'https://api.weixin.qq.com/sns/oauth2/refresh_token';
+
+    /**
+     * @inheritdoc
+     */
     public $apiBaseUrl = 'https://api.weixin.qq.com';
+
+    /**
+     * @var bool 是否使用openid
+     */
+    public $useOpenId = true;
+
+    /**
+     * @var bool 是否是微信公众平台
+     */
+    public $useMp = false;
 
     /**
      * @inheritdoc
@@ -38,10 +54,19 @@ class Wechat extends OAuth2
     public function init()
     {
         parent::init();
-        if ($this->scope === null) {
-            $this->scope = implode(',', [
-                'snsapi_login',
-            ]);
+        if ($this->useMp) {
+            $this->authUrl = 'https://open.weixin.qq.com/connect/oauth2/authorize';
+            if ($this->scope === null) {
+                $this->scope = implode(',', [
+                    'snsapi_userinfo',
+                ]);
+            }
+        } else {
+            if ($this->scope === null) {
+                $this->scope = implode(',', [
+                    'snsapi_login',
+                ]);
+            }
         }
     }
 
@@ -50,19 +75,45 @@ class Wechat extends OAuth2
      */
     protected function defaultNormalizeUserAttributeMap()
     {
-        return [
-            'id' => 'openid',
-            'username' => 'nickname',
-        ];
+        if ($this->useOpenId) {
+            return [
+                'id' => 'openid',
+                'username' => 'nickname',
+            ];
+        } else {
+            return [
+                'id' => 'unionid',
+                'username' => 'nickname',
+            ];
+        }
     }
 
     /**
-     * @inheritdoc
+     * Composes user authorization URL.
+     * @param array $params additional auth GET params.
+     * @return string authorization URL.
      */
     public function buildAuthUrl(array $params = [])
     {
-        $params = array_merge($params, ['appid' => $this->clientId]);
-        return parent::buildAuthUrl($params);
+        $defaultParams = [
+            'appid' => $this->clientId,
+            'redirect_uri' => $this->getReturnUrl(),
+            'response_type' => 'code',
+        ];
+        if (!empty($this->scope)) {
+            $defaultParams['scope'] = $this->scope;
+        }
+
+        if ($this->validateAuthState) {
+            $authState = $this->generateAuthState();
+            $this->setState('authState', $authState);
+            $defaultParams['state'] = $authState;
+        }
+        $authUrl = $this->composeUrl($this->authUrl, array_merge($defaultParams, $params));
+        if ($this->useMp) {
+            return $authUrl . '#wechat_redirect';
+        }
+        return $authUrl;
     }
 
     /**
@@ -88,6 +139,20 @@ class Wechat extends OAuth2
     }
 
     /**
+     * Applies client credentials (e.g. [[clientId]] and [[clientSecret]]) to the HTTP request instance.
+     * This method should be invoked before sending any HTTP request, which requires client credentials.
+     * @param \yii\httpclient\Request $request HTTP request instance.
+     * @since 2.1.3
+     */
+    protected function applyClientCredentialsToRequest($request)
+    {
+        $request->addData([
+            'appid' => $this->clientId,
+            'secret' => $this->clientSecret,
+        ]);
+    }
+
+    /**
      * 返回OpenId
      * @return mixed
      */
@@ -102,6 +167,33 @@ class Wechat extends OAuth2
     protected function initUserAttributes()
     {
         return $this->api('sns/userinfo', 'GET');
+    }
+
+    /**
+     * Gets new auth token to replace expired one.
+     * @param OAuthToken $token expired auth token.
+     * @return OAuthToken new auth token.
+     */
+    public function refreshAccessToken(OAuthToken $token)
+    {
+        $params = [
+            'grant_type' => 'refresh_token'
+        ];
+        $params = array_merge($token->getParams(), $params);
+
+        $request = $this->createRequest()
+            ->setMethod('POST')
+            ->setUrl($this->refreshTokenUrl)
+            ->setData($params);
+
+        $this->applyClientCredentialsToRequest($request);
+
+        $response = $this->sendRequest($request);
+
+        $token = $this->createToken(['params' => $response]);
+        $this->setAccessToken($token);
+
+        return $token;
     }
 
     /**
